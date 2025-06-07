b0e568af0d2d5fb404df75980dcdec4a9c6b0a80
Hakdooooooooog <hicap.darenzjasper@gmail.com>
2025-06-07 12:58:17 +0800

    Initialize next js project

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import SearchBar from "@/components/searchbar";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
+      <SearchBar />
+    </div>
+  );
+}
